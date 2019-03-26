98d301513db6f92314e3e4edc66d841d7a205ccd
Nicholas Eubanks <nick-2013@live.com>
2019-03-26 17:29:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
   elsif int % 5 == 0 
     "Buzz"
   else (int % 3 == 0) && (int % 5 == 0)
